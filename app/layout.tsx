fe6ec9f586a7f4789031572c77526bf8e4ee257b
Argeer78 <sgouros2305@gmail.com>
2026-01-02 10:23:37 +0200

    updated fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'AW-17847132623');
           `}
         </Script>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '345527236565069');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=345527236565069&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {/* ✅ Make language available everywhere (including /auth) */}
         <UiLanguageProvider>
           <FocusProvider>
